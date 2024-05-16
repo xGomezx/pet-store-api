e160637e810a230d63f35ba13a7d842d81ff5193
Juan Jose Gomez <juanjosegomez182@gmail.com>
2024-05-16 13:11:47 -0500

    conectar base de datos mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import { envs } from "./config";
+import { MongoDatabase } from "./data/mongodb"; //2
 import { AppRoutes } from "./presentacion/router"; //1
 import { Server } from "./presentacion/server";
 
-(()=> {
-    main();
-  })()
-  
-  async function main (){
+(() => {
+  main();
+})();
+
+async function main() {
   // todo: await base de datos
-  
+  await MongoDatabase.connect({//1
+    //3
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
+
   // todo: inicio de nuestro server
-      new Server({
-        port: envs.PORT,
-        routes: AppRoutes.routes //2
-      }).start();
-  }
+  new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes, //2
+  }).start();
+}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,9 +1,9 @@
-// importamos las librerias 
-import 'dotenv/config';
-import { get} from 'env-var';
+import "dotenv/config";
+import { get } from "env-var";
 
-// creamos nuestro enum que manejera nuestra envs
 export const envs = {
+  PORT: get("PORT").required().asPortNumber(),
 
-    PORT: get('PORT').required().asPortNumber(),
-}
+  MONGO_URL: get("MONGO_URL").required().asString(), //1
+  MONGO_DB_NAME: get("MONGO_DB_NAME").required().asString(), //2
+};
